ea63e29c970fbcd72bf637b2da81b1dba6829148
rohit <rhtry.tech@gmail.com>
2023-02-11 22:15:22 +0530

    adding new files !

---
+++ cad-yogeshwar-code/index.js
@@ -0,0 +1,63 @@
+const drawBtn = document.querySelector("#drawBtn");
+const rectList = document.querySelector("#rectList");
+let isDrawing = false;
+let startX, startY;
+let currentRect;
+
+drawBtn.addEventListener("click", () => {
+  isDrawing = true;
+});
+
+rectList.addEventListener("click", (e) => {
+  const selectedRect = document.querySelector(".selected");
+  if (selectedRect) {
+    selectedRect.classList.remove("selected");
+  }
+  const rectId = e.target.dataset.rectId;
+  if (rectId) {
+    const rect = document.querySelector(`[data-rect="${rectId}"]`);
+    rect.classList.add("selected");
+  }
+});
+
+document.addEventListener("mousedown", (e) => {
+  if (isDrawing) {
+    startX = e.clientX;
+    startY = e.clientY;
+    currentRect = document.createElement("div");
+    currentRect.classList.add("rect");
+    currentRect.style.left = startX + "px";
+    currentRect.style.top = startY + "px";
+    document.body.appendChild(currentRect);
+  }
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (currentRect && isDrawing) {
+    const deltaX = e.clientX - startX;
+    const deltaY = e.clientY - startY;
+    currentRect.style.width = deltaX + "px";
+    currentRect.style.height = deltaY + "px";
+  }
+});
+
+document.addEventListener("mouseup", (e) => {
+  if (currentRect) {
+    isDrawing = false;
+    const rect = {
+      x: parseInt(currentRect.style.left),
+      y: parseInt(currentRect.style.top),
+      width: parseInt(currentRect.style.width),
+      height: parseInt(currentRect.style.height),
+    };
+    const rectId = Date.now();
+    currentRect.setAttribute("data-rect", rectId);
+    const rectItem = document.createElement("li");
+    rectItem.innerHTML = `Rectangle (${rect.x}, ${rect.y}) - (${
+      rect.x + rect.width
+    }, ${rect.y + rect.height})`;
+    rectItem.setAttribute("data-rect-id", rectId);
+    rectList.appendChild(rectItem);
+    currentRect = null;
+  }
+});
