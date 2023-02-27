72aae9942c600edcb805ca67a21d6026e7d65e7c
rohit <rhtry.tech@gmail.com>
2023-02-27 19:16:15 +0530

    adding dragging module !

--- drag-testing/index.js
+++ drag-testing/index.js
@@ -3,13 +3,28 @@ var children = document.getElementsByClassName("child");
 var selectedChild = null;
 var isDragging = false;
 var mouseOffset = {x: 0, y: 0};
+let allowedToDrag = false;
 
+const dragBtn = document.querySelector("#dragBtn");
+
+dragBtn.addEventListener("click", () => {
+  console.log("Start Dragging !");
+  allowedToDrag = true;
+  console.log(allowedToDrag);
+
+  dragBtn.style.backgroundColor = "green";
+});
+
+// if (allowedToDrag) {
 for (var i = 0; i < children.length; i++) {
   children[i].addEventListener("mousedown", function (event) {
-    selectedChild = event.target;
-    isDragging = true;
-    mouseOffset.x = event.offsetX;
-    mouseOffset.y = event.offsetY;
+    if (allowedToDrag) {
+      selectedChild = event.target;
+      console.log(selectedChild);
+      isDragging = true;
+      mouseOffset.x = event.offsetX;
+      mouseOffset.y = event.offsetY;
+    }
   });
 }
 
@@ -25,3 +40,4 @@ document.addEventListener("mousemove", function (event) {
 document.addEventListener("mouseup", function (event) {
   isDragging = false;
 });
+// }
